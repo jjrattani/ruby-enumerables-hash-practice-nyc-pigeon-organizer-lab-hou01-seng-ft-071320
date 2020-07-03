588e446c0512136f1b4c71a508f3d27812476ba7
Jessica  Rattani <jjrattani@gmail.com>
2020-07-03 16:01:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,6 +17,20 @@ def nyc_pigeon_organizer(data)
   end
   
   # Now we need to loop over the keys in the data given to us.
+  pivoted_hash.each do |parent_key, parent_hash|
+    # parent_key will either be color, gender or lives.  parent_hash is color => purple for example.
+    
+    # Loop over the parent_hashes.  This will return the nested keys in the hash, such as color -> purple -> array_of_names for example.
+    parent_hash.each do |child_key, child_array|
+      # Now we have color, gender, or lives in parent_key
+      # Now we have color -> purple, color -> white, gender -> male, gender -> female, and lives and its children available to us.
+      # We need to loop over each child array to get the names of each corresponding pigeon and store them into our pivoted_hash
+      child_array.each do |pigeon_name|
+        #pigeon name consists of "Theo", or "Peter. Jr".  These names are keys in our pivoted hash.  
+        pivoted_hash[pigeon_name][parent_key].push(child_key)
+      end
+    end
+  end
   pivoted_hash
 end
 
